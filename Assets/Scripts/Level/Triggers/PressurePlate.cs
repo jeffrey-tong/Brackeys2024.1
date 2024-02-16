adfda08d1a04a4b0138918a1d3af3c6d91e673a0
jeffrey-tong <jeffreytong41@gmail.com>
2024-02-16 12:15:40 -0500

    Pressure plate can now be triggered with the "Triggerables" Tag, ie metal block

--- Assets/Scripts/Level/Triggers/PressurePlate.cs
+++ Assets/Scripts/Level/Triggers/PressurePlate.cs
@@ -16,6 +16,14 @@ public class PressurePlate : BaseTrigger
             if (collision.TryGetComponent(out PlayerController player))
                 Trigger(player);
         }
+
+        if (collision.CompareTag("Triggerable"))
+        {
+            if (!collision.isTrigger)
+            {
+                Trigger(null);
+            }   
+        }
     }
 
     protected override void OnTriggerExit2D(Collider2D collision)
