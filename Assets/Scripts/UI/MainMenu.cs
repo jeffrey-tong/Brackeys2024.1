106c5f22ec3dadf43e60adc20352c25abc6a82ff
Ryan Salam <Ryan@agni.com>
2024-02-17 21:55:54 -0500

    Visual Effect stuff

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private Button startGameBtn;
+    [SerializeField] private string levelSceneName;
+
+    private void Start()
+    {
+        startGameBtn.onClick.AddListener(OnStartGameBtnClicked);
+    }
+
+    private void OnStartGameBtnClicked()
+    {
+        TransitionManager.Instance.LoadScene(levelSceneName);
+    }
+}
