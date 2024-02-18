7ed5faf2eb788f1874f17ae2d6d2ff9a6ff12210
jeffrey-tong <jeffreytong41@gmail.com>
2024-02-17 23:30:26 -0500

    Platform adjusted for new images

--- Assets/Scripts/Level/Interactables/VisibilityPlatform.cs
+++ Assets/Scripts/Level/Interactables/VisibilityPlatform.cs
@@ -6,8 +6,20 @@ public class VisibilityPlatform : BaseInteractable
 {
     [SerializeField] private bool isVisible = true;
 
+    private SpriteRenderer sr;
+    private BoxCollider2D boxCollider;
+
+    [SerializeField] private float platformLength;
+    [SerializeField] private float platformWidth;
+
     private void Start()
     {
+        sr = GetComponent<SpriteRenderer>();
+        boxCollider = GetComponent<BoxCollider2D>();
+
+        sr.size = new Vector2(platformLength, platformWidth);
+        boxCollider.size = new Vector2(platformLength, platformWidth);
+
         gameObject.SetActive(isVisible);
     }
 
@@ -16,5 +28,13 @@ public class VisibilityPlatform : BaseInteractable
         base.Activate();
         isVisible = !isVisible;
         gameObject.SetActive(isVisible);
+    }
+
+    private void OnValidate()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        boxCollider = GetComponent<BoxCollider2D>();
+        sr.size = new Vector2(platformLength, platformWidth);
+        boxCollider.size = new Vector2(platformLength, platformWidth);
     }
 }
