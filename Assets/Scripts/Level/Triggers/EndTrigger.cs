b464ba5077350830f6bafe0de95dd3414af661ab
jeffrey-tong <jeffreytong41@gmail.com>
2024-02-17 22:33:29 -0500

    Stuff added
    Door particles added,
    End scene started,
    Visibility platform can start invisible

---
+++ Assets/Scripts/Level/Triggers/EndTrigger.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndTrigger : BaseTrigger
+{ 
+    public override void Trigger(PlayerController controller)
+    {
+        EndGame();
+    }
+
+    private void EndGame()
+    {
+         TransitionManager.Instance.LoadScene("EndScene");
+    }
+}
